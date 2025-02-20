45e21ad334797f3d38880ea2b953212a85076f12
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-02-20 01:35:57 -0300

    se agrego el CartWidget

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+
+import './CartWidget.css'; 
+
+import { LuShoppingBasket } from "react-icons/lu";
+
+function NavBar() {
+    return(
+        <button className='button-navbar' id='shop-button'><LuShoppingBasket /></button>
+    ); 
+}
+export default NavBar; 
