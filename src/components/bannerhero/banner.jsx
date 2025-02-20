bea23a7ed0d96ad037f19fb1aea3c5f55a1e61bc
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-02-19 23:28:37 -0300

    Se creo el archivo Banner

---
+++ src/components/bannerhero/banner.jsx
@@ -0,0 +1,14 @@
+
+import './banner.css'; 
+
+
+function NavBar() {
+    return(
+        
+      <section className='sectionHero'>
+        <img src="../public/images/bannerHero.jpg" alt="Banner de la sección Principal" />
+      </section>
+      
+    ); 
+}
+export default NavBar; 
